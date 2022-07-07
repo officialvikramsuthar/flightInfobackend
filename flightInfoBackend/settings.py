afd05cc186c32298edff3b223c454b44f14380aa
vikram_suthar9890 <vikram.suthar@kitomba.com>
2022-07-07 23:04:35 +0530

    added host

--- flightInfoBackend/settings.py
+++ flightInfoBackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-yv9-1-*gsio+&8xkt@im73327&)+n@xro%4-$n(dvlz@5hl*t$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['flightbackend.herokuapp.com']
 
 
 # Application definition
